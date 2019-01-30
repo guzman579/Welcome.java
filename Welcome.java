d4ddcd49d5f6e2b324fd03df4b82a127dad8c0b4
Irene <35508977+guzman579@users.noreply.github.com>
2019-01-29 19:13:28 -0800

    Create Welcome.java

---
+++ Welcome.java
@@ -0,0 +1,12 @@
+// Welcome.java
+// Printing text in two lines
+
+public class Welcome 
+{
+   // main method begins execution of Java application
+   public static void main(String[] args)
+   {
+      System.out.print("Welcome to ");
+      System.out.println("Java Programming!");
+   } // end main
+} // end class 
